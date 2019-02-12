e6b2a730676488074f7176771f57fc95080f8eae
Aurora Wright <AuroraWright@users.noreply.github.com>
2019-02-12 16:21:52 +0100

    Fix build on recent AOSP trees, fix italian translation

--- Android.mk
+++ Android.mk
@@ -5,5 +5,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := MyPhoneNumber
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_JAVA_LIBRARIES += telephony-common
 
 include $(BUILD_PACKAGE)
